41c15aa25eaed3e88a5ad6da329b59736b3d2dbe
reza <rezajafarian723@gmail.com>
2022-08-25 07:46:48 -0700

    add signup

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    Image = models.ImageField(upload_to='images', blank=True, null=True)
+    Name = models.CharField(max_length=50)
+    # Username = models.CharField(max_length=40)
+    Website = models.CharField(max_length=50, blank=True, null=True)
+    Bio = models.CharField(max_length=100, blank=True, null=True)
+    Followings = models.ManyToManyField('self', symmetrical=False, related_name='followers', default=0)
